53c6d2db7e7e73ea8a1cfe3d0bf5c62e5458d3f7
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-03-31 01:51:08 +0300

    uploading file

---
+++ Working with Abstraction/01.Raw Data/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_RawData
+{
+    public class Car
+    {
+        private readonly Tire[] tires;
+
+        public Car(string model, Engine engine, Cargo cargo, Tire[] tiresFromInput)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+            this.tires = tiresFromInput;
+        }
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public IReadOnlyCollection<Tire> Tires
+        {
+            get
+            {
+                return this.tires;
+            }
+        }
+    }
+}
