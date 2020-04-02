531814c81c92cf697b6d4d9bfc7642287359c3e8
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-03 00:58:13 +0300

    Add files via upload

---
+++ Working with Abstraction/P04.Hospital/Department.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P04_Hospital
+{
+    public class Department
+    {
+        private List<Room> rooms;
+
+        public Department(string name)
+        {
+            this.rooms = new List<Room>();
+            this.Name = name;
+            InitializeRooms();
+        }
+
+        public string Name { get; set; }
+        public IReadOnlyCollection<Room> Rooms => this.rooms;
+
+        public Room GetFirstFreeRoom()
+        {
+            return this.rooms.First(x => x.Count < 3);
+        }
+        public void InitializeRooms()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                this.rooms.Add(new Room(i));
+            }
+        }
+    }
+}
