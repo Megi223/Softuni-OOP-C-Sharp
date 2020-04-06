68b376440298de7e4f7090332e4a1afe1e61b6a5
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-06 10:06:40 +0300

    uploading files

---
+++ Exams/C# OOP Exam - 15 Dec 2019/P01+P02-Structure and Business Logic/Models/Aquariums/Aquarium.cs
@@ -0,0 +1,67 @@
+﻿using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+    public abstract class Aquarium : IAquarium
+    {
+        private string name;
+        private List<IDecoration> decorations;
+        private List<IFish> fish;
+
+        protected Aquarium(string name,int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.fish = new List<IFish>();
+            this.decorations = new List<IDecoration>();
+
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value)||String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAquariumName);
+                }
+                this.name = value;
+            }
+        }
+
+
+        public int Capacity { get; private set; }
+
+        public int Comfort => this.Decorations.Sum(d => d.Comfort);
+
+        public ICollection<IDecoration> Decorations => this.decorations;
+
+
+        public ICollection<IFish> Fish => this.fish;
+
+        public abstract void AddDecoration(IDecoration decoration);
+
+
+        public abstract void AddFish(IFish fish);
+        
+
+        public abstract void Feed();
+        
+
+        public abstract string GetInfo();
+        
+
+        public abstract bool RemoveFish(IFish fish);
+        
+    }
+}
---
+++ Exams/C# OOP Exam - 15 Dec 2019/P01+P02-Structure and Business Logic/Models/Aquariums/FreshwaterAquarium.cs
@@ -0,0 +1,69 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+    public class FreshwaterAquarium : Aquarium
+    {
+        public FreshwaterAquarium(string name) : base(name,50)
+        {
+        }
+
+        public override void AddDecoration(IDecoration decoration)
+        {
+            this.Decorations.Add(decoration);
+        }
+
+        public override void AddFish(IFish fish)
+        {
+            if (this.Fish.Count + 1 > this.Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughCapacity);
+            }
+            this.Fish.Add(fish);
+        }
+
+        public override void Feed()
+        {
+            foreach (var fish in this.Fish)
+            {
+                fish.Eat();
+            }
+        }
+
+        public override string GetInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Name} (FreshwaterAquarium):");
+            string secondLine = "";
+            if (this.Fish.Count == 0)
+            {
+                secondLine = "none";
+            }
+            else
+            {
+                secondLine = String.Join(", ", this.Fish.Select(f=>f.Name));
+            }
+            sb.AppendLine($"Fish: {secondLine}")
+                .AppendLine($"Decorations: {this.Decorations.Count}")
+                .AppendLine($"Comfort: {this.Comfort}");
+            return sb.ToString().TrimEnd();
+        }
+
+        public override bool RemoveFish(IFish fish)
+        {
+            IFish wantedFish = this.Fish.FirstOrDefault(f => f == fish);
+            if (wantedFish == null)
+            {
+                return false;
+            }
+            this.Fish.Remove(wantedFish);
+            return true;
+        }
+    }
+}
