24aa2b49208d7b5ff2ff3566518b8de5a3e8b8b9
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-08 11:37:31 +0300

    uploading files

---
+++ Exams/C# OOP Exam - 15 Dec 2019/P01+P02-Structure and Business Logic/Repositories/DecorationRepository.cs
@@ -0,0 +1,43 @@
+﻿using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Repositories
+{
+    public class DecorationRepository : IRepository<IDecoration>
+    {
+        private List<IDecoration> models;
+
+        public DecorationRepository()
+        {
+            this.models = new List<IDecoration>();
+        }
+        
+        public IReadOnlyCollection<IDecoration> Models => this.models.AsReadOnly();
+
+        public void Add(IDecoration model)
+        {
+            this.models.Add(model);
+        }
+
+        public IDecoration FindByType(string type)
+        {
+            IDecoration wantedDecoration = this.models.FirstOrDefault(d => d.GetType().Name == type);
+            return wantedDecoration;
+        }
+
+        public bool Remove(IDecoration model)
+        {
+            IDecoration wantedDecoration = this.models.FirstOrDefault(d => d == model);
+            if (wantedDecoration == null)
+            {
+                return false;
+            }
+            this.models.Remove(wantedDecoration);
+            return true;
+        }
+    }
+}
