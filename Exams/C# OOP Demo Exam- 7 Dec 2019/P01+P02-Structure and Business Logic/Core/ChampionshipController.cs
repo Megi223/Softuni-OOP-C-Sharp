d8cd71873da99f6508dc54a3bad492d109d5e313
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-11 11:20:02 +0300

    uploading files

---
+++ Exams/C# OOP Demo Exam- 7 Dec 2019/P01+P02-Structure and Business Logic/Core/ChampionshipController.cs
@@ -0,0 +1,152 @@
+﻿using MXGP.Core.Contracts;
+using MXGP.Models.Motorcycles;
+using MXGP.Models.Motorcycles.Contracts;
+using MXGP.Models.Races;
+using MXGP.Models.Races.Contracts;
+using MXGP.Models.Riders;
+using MXGP.Models.Riders.Contracts;
+using MXGP.Repositories;
+using MXGP.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Collections.ObjectModel;
+
+namespace MXGP.Core
+{
+    public class ChampionshipController : IChampionshipController
+    {
+        private MotorcycleRepository motorcycleRepository;
+        private RiderRepository riderRepository;
+        private RaceRepository raceRepository;
+        private List<IRider> riders;
+
+
+        public ChampionshipController()
+        {
+            this.motorcycleRepository = new MotorcycleRepository();
+            this.riderRepository = new RiderRepository();
+            this.raceRepository = new RaceRepository();
+            this.riders = new List<IRider>();
+        }
+        public void Ordering(Func<IRider, double> src)
+        {
+            riders = riders.OrderByDescending(src).ToList();
+        }
+        public string AddMotorcycleToRider(string riderName, string motorcycleModel)
+        {
+            IRider rider = riderRepository.Models.FirstOrDefault(r => r.Name == riderName);
+            IMotorcycle motorcycle = this.motorcycleRepository.Models.FirstOrDefault(m => m.Model == motorcycleModel);
+            if (rider==null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.RiderNotFound, riderName));
+            }
+            if (motorcycle == null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.MotorcycleNotFound, motorcycleModel));
+            }
+            rider.AddMotorcycle(motorcycle);
+            return String.Format(OutputMessages.MotorcycleAdded,rider.Name,motorcycle.Model);
+        }
+
+        public string AddRiderToRace(string raceName, string riderName)
+        {
+            
+            if ((raceRepository.Models.FirstOrDefault(r => r.Name == raceName)) == null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.RaceNotFound, raceName));
+            }
+            if (riderRepository.Models.FirstOrDefault(r=>r.Name==riderName)==null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.RiderNotFound, riderName));
+            }
+            IRider rider = riderRepository.Models.FirstOrDefault(r => r.Name == riderName);
+            IRace race = raceRepository.Models.FirstOrDefault(r => r.Name == raceName);
+            race.AddRider(rider);
+            return String.Format(OutputMessages.RiderAdded, rider.Name, race.Name);
+
+
+        }
+
+        public string CreateMotorcycle(string type, string model, int horsePower)
+        {
+            if ((this.motorcycleRepository.GetByName(model) != null))
+            {
+                throw new ArgumentException(String.Format(ExceptionMessages.MotorcycleExists, model));
+            }
+            IMotorcycle motorcycle = null;
+            if(type== "Speed")
+            {
+                motorcycle = new SpeedMotorcycle(model, horsePower);
+            }
+            else if(type== "Power")
+            {
+                motorcycle = new PowerMotorcycle(model, horsePower);
+            }
+            
+            this.motorcycleRepository.Add(motorcycle);
+            return String.Format(OutputMessages.MotorcycleCreated,motorcycle.GetType().Name,motorcycle.Model);
+            
+        }
+
+        public string CreateRace(string name, int laps)
+        {
+            if (raceRepository.Models.FirstOrDefault(r=>r.Name==name &&  r.Laps==laps)!=null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.RaceExists, name));
+            }
+            IRace race = new Race(name, laps);
+            this.raceRepository.Add(race);
+            return String.Format(OutputMessages.RaceCreated, race.Name);
+        }
+
+        public string CreateRider(string riderName)
+        {
+            IRider rider = this.riderRepository.Models.FirstOrDefault(r => r.Name == riderName);
+            if (rider != null)
+            {
+                throw new ArgumentException(String.Format(ExceptionMessages.RiderExists, riderName));
+            }
+            IRider newRider = new Rider(riderName);
+            this.riderRepository.Add(newRider);
+            return String.Format(OutputMessages.RiderCreated, newRider.Name);
+        }
+
+        public string StartRace(string raceName)
+        {
+            IRace race = raceRepository.Models.FirstOrDefault(r => r.Name == raceName);
+            if ( race == null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.RaceNotFound, raceName));
+            }
+            if (race.Riders.Count < 3)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.RaceInvalid, raceName,3));
+            }
+
+
+            
+            riders = race.Riders.ToList();
+            Ordering(r => r.Motorcycle.CalculateRacePoints(race.Laps));
+            string first = riders.First().Name;
+            string sec = riders.Skip(1).First().Name;
+            string third = riders.Skip(2).First().Name;
+
+
+            StringBuilder sb = new StringBuilder();
+           
+
+            sb.AppendLine(String.Format(OutputMessages.RiderFirstPosition, first, race.Name))
+            .AppendLine(String.Format(OutputMessages.RiderSecondPosition, sec, race.Name))
+            .AppendLine(String.Format(OutputMessages.RiderThirdPosition,third, race.Name));
+            raceRepository.Remove(race);
+            return sb.ToString().TrimEnd();
+
+        }
+        
+
+        
+    }
+}
---
+++ Exams/C# OOP Demo Exam- 7 Dec 2019/P01+P02-Structure and Business Logic/Core/Engine.cs
@@ -0,0 +1,93 @@
+﻿using MXGP.Core.Contracts;
+using MXGP.IO;
+using MXGP.IO.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MXGP.Core
+{
+    public class Engine : IEngine
+    {
+        private ChampionshipController championshipController;
+        private IWriter writer;
+        private IReader reader;
+        public Engine()
+        {
+            this.championshipController = new ChampionshipController();
+            this.writer = new ConsoleWriter();
+            this.reader = new ConsoleReader();
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                string[] input = reader.ReadLine().Split();
+                if (input[0] == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+                //
+                try
+                {
+                    //
+                    string result = string.Empty;
+
+                    if (input[0] == "CreateRider")
+                    {
+                        string name = input[1];
+                       
+
+                        result = championshipController.CreateRider(name);
+                    }
+                    else if (input[0] == "CreateMotorcycle")
+                    {
+                        string type = input[1];
+                        string model = input[2];
+                        int horsePower =int.Parse(input[3]);
+
+                        result = championshipController.CreateMotorcycle(type,model, horsePower);
+                    }
+                    else if (input[0] == "AddMotorcycleToRider")
+                    {
+                        string riderName = input[1];
+                        string motorName = input[2];
+
+                        result = championshipController.AddMotorcycleToRider(riderName, motorName);
+                    }
+                    else if (input[0] == "AddRiderToRace")
+                    {
+                        string raceName = input[1];
+                        string riderName = input[2];
+
+
+                        result = championshipController.AddRiderToRace(raceName, riderName);
+                    }
+                    else if (input[0] == "CreateRace")
+                    {
+                        string name = input[1];
+                        int laps = int.Parse(input[2]);
+
+
+                        result = championshipController.CreateRace(name,laps);
+                    }
+                    else if (input[0] == "StartRace")
+                    {
+                        string name = input[1];
+
+                        result = championshipController.StartRace(name);
+                    }
+                    
+
+                    writer.WriteLine(result);
+                    //
+                }
+                catch (Exception ex)
+                {
+                    writer.WriteLine(ex.Message);
+                }
+                //
+            }
+        }
+    }
+}
