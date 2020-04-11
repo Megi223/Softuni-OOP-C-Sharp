adfe12aa8872cb64e856be370d4b9d82ffadbfd9
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-11 11:20:48 +0300

    uploading files

---
+++ Exams/C# OOP Demo Exam- 7 Dec 2019/P01+P02-Structure and Business Logic/Models/Motorcycles/PowerMotorcycle.cs
@@ -0,0 +1,35 @@
+﻿using MXGP.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MXGP.Models.Motorcycles
+{
+    public class PowerMotorcycle : Motorcycle
+    {
+        private int horsePower;
+        
+        public PowerMotorcycle(string model, int horsePower) : base(model, horsePower, 450)
+        {
+            
+        }
+
+        public override int HorsePower 
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            protected set
+            {
+                if(value<70 || value > 100)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidHorsePower, value));
+                }
+                this.horsePower = value;
+            }
+        }
+       
+        
+    }
+}
