6e28ef30bdae5ded22676bd253ffce22fb706be7
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-17 10:44:27 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 14 April 2019/P01+P02-Structure and Business Logic/Common/ExceptionMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MortalEngines.Common
+{
+    public class ExceptionMessages
+    {
+        public const string InvalidMachineName = "Machine name cannot be null or empty.";
+
+        public const string PilonCannotBeNull = "Pilot cannot be null.";
+
+        public const string TargetCannotBeNull = "Target cannot be null";
+
+        public const string InvalidPilotName = "Pilot name cannot be null or empty string.";
+
+        public const string MachineCannotBeNull = "Null machine cannot be added to the pilot.";
+
+        
+    }
+}
