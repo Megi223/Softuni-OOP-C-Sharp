7503db55b9f825b455de335fedbc03300754552f
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-22 02:33:04 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 15 August 2019/P01+P02-Structure and Business Logic/Core/Controller.cs
@@ -0,0 +1,111 @@
+﻿
+using SpaceStation.Core.Contracts;
+using SpaceStation.Models.Astronauts;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission;
+using SpaceStation.Models.Planets;
+using SpaceStation.Repositories;
+using SpaceStation.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Core
+{
+    public class Controller : IController
+    {
+        private AstronautRepository astronautRepository;
+        private PlanetRepository planetRepository;
+        private int exploredPlanets;
+        public Controller()
+        {
+            this.astronautRepository = new AstronautRepository();
+            this.planetRepository = new PlanetRepository();
+        }
+        public string AddAstronaut(string type, string astronautName)
+        {
+            if(type!= "Biologist" && type!= "Geodesist"&& type!= "Meteorologist")
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidAstronautType);
+            }
+            IAstronaut astronaut = null;
+            if(type == "Biologist")
+            {
+                astronaut = new Biologist(astronautName);
+            }
+            else if(type== "Geodesist")
+            {
+                astronaut = new Geodesist(astronautName);
+            }
+            else if (type == "Meteorologist")
+            {
+                astronaut = new Meteorologist(astronautName);
+            }
+            this.astronautRepository.Add(astronaut);
+            return String.Format(OutputMessages.AstronautAdded, type, astronautName);
+        }
+
+        public string AddPlanet(string planetName, params string[] items)
+        {
+            Planet planet = new Planet(planetName);
+            foreach (var item in items)
+            {
+                planet.Items.Add(item);
+            }
+            this.planetRepository.Add(planet);
+            return String.Format(OutputMessages.PlanetAdded, planetName);
+        }
+
+        public string ExplorePlanet(string planetName)
+        {
+            IPlanet wantedPlanet = this.planetRepository.FindByName(planetName);
+            ICollection<IAstronaut> suitableAstronauts = this.astronautRepository.Models.Where(a => a.Oxygen > 60).ToList();
+            if (suitableAstronauts.Count == 0)
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidAstronautCount);
+            }
+            Mission mission = new Mission();
+            mission.Explore(wantedPlanet, suitableAstronauts);
+            exploredPlanets++;
+            //int countDeadAstronauts = suitableAstronauts.Where(a => a == null).Count();
+            var deadAstronauts = suitableAstronauts
+                .Count(x => !x.CanBreath);
+            return String.Format(OutputMessages.PlanetExplored, planetName, deadAstronauts);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{exploredPlanets} planets were explored!")
+                .AppendLine("Astronauts info:");
+            foreach (var astronaut in this.astronautRepository.Models)
+            {
+                sb.AppendLine($"Name: {astronaut.Name}")
+                    .AppendLine($"Oxygen: {astronaut.Oxygen}");
+                string thirdLine = null;
+                if (astronaut.Bag.Items.Count ==0)
+                {
+                    thirdLine = "none";
+                }
+                else
+                {
+                    thirdLine = String.Join(", ", astronaut.Bag.Items);
+                }
+                sb.AppendLine($"Bag items: {thirdLine}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RetireAstronaut(string astronautName)
+        {
+            IAstronaut wantedAstronaut = this.astronautRepository.Models.FirstOrDefault(a => a.Name == astronautName);
+            if (wantedAstronaut == null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.InvalidRetiredAstronaut, astronautName));
+            }
+            this.astronautRepository.Remove(wantedAstronaut);
+            return String.Format(OutputMessages.AstronautRetired, astronautName);
+        }
+    }
+}
---
+++ Exams/C# OOP Exam - 15 August 2019/P01+P02-Structure and Business Logic/Core/Engine.cs
@@ -0,0 +1,76 @@
+﻿using SpaceStation.Core.Contracts;
+using SpaceStation.IO;
+using SpaceStation.IO.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Core
+{
+    public class Engine : IEngine
+    {
+        private IWriter writer;
+        private IReader reader;
+        private IController controller;
+
+        public Engine()
+        {
+            this.writer = new Writer();
+            this.reader = new Reader();
+            this.controller = new Controller();
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                string result = string.Empty;
+                var input = reader.ReadLine().Split();
+                if (input[0] == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+                try
+                {
+                    if (input[0] == "AddAstronaut")
+                    {
+                        string type = input[1];
+                        string name = input[2];
+                        result = controller.AddAstronaut(type, name);
+                    }
+                    else if (input[0] == "AddPlanet")
+                    {
+                        string name = input[1];
+                        ICollection<string> collection = new List<string>();
+                        for (int i = 2; i < input.Length; i++)
+                        {
+                            collection.Add(input[i]);
+                        }
+                        string[] arr = collection.ToArray();
+                        result = controller.AddPlanet(name, arr);
+
+                    }
+                    else if (input[0] == "RetireAstronaut")
+                    {
+                        string name = input[1];
+                        result = controller.RetireAstronaut(name);
+                    }
+                    else if (input[0] == "ExplorePlanet")
+                    {
+                        string name = input[1];
+                        result = controller.ExplorePlanet(name);
+                    }
+                    else if(input[0] == "Report")
+                    {
+                        result = controller.Report();
+                    }
+                    this.writer.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    writer.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
