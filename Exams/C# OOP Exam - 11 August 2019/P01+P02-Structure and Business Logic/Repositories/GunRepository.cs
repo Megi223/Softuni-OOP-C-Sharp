f6d59fd451d40b934e47ad33c7fa1fefb73e1f2b
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-21 10:24:39 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 11 August 2019/P01+P02-Structure and Business Logic/Repositories/GunRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ViceCity.Models.Guns;
+using ViceCity.Models.Guns.Contracts;
+using ViceCity.Repositories.Contracts;
+
+namespace ViceCity.Repositories
+{
+    public class GunRepository : IRepository<IGun>
+    {
+        private List<IGun> models;
+        public GunRepository()
+        {
+            this.models = new List<IGun>();   
+        }
+        public IReadOnlyCollection<IGun> Models => this.models.AsReadOnly();
+
+        public void Add(IGun model)
+        {
+            this.models.Add(model);
+        }
+
+        public IGun Find(string name)
+        {
+           return this.models.FirstOrDefault(g => g.Name == name);
+        }
+
+        public bool Remove(IGun model) => this.models.Remove(model);
+        
+    }
+}
