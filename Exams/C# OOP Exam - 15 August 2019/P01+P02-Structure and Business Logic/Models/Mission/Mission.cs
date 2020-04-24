c4c9a10aac3175f8c2c7299d8534226ded81c702
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-24 10:05:58 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 15 August 2019/P01+P02-Structure and Business Logic/Models/Mission/Mission.cs
@@ -0,0 +1,48 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission;
+using SpaceStation.Models.Planets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Models.Mission
+
+{
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet,ICollection<IAstronaut> astronauts)
+        {
+            while (true)
+            {
+                var astronaut = astronauts
+                    .FirstOrDefault(x => x.CanBreath);
+
+                if (astronaut == null)
+                {
+                    break;
+                }
+
+                var item = planet
+                    .Items
+                    .FirstOrDefault();
+
+                if (item == null)
+                {
+                    break;
+                }
+
+                astronaut.Breath();
+
+                astronaut
+                    .Bag
+                    .Items
+                    .Add(item);
+
+                planet
+                    .Items
+                    .Remove(item);
+            }
+        }
+    }
+}
