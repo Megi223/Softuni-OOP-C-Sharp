b6502b31878d27bbbb4e7ba6d20a72d0abbada8f
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-25 13:10:41 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 15 August 2019/P01+P02-Structure and Business Logic/Models/Planets/Planet.cs
@@ -0,0 +1,37 @@
+﻿using SpaceStation.Models.Planets;
+using SpaceStation.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Planets
+
+{
+    public class Planet : IPlanet
+    {
+        private string name;
+        private List<string> items;
+        public Planet(string name)
+        {
+            this.Name = name;
+            this.items = new List<string>();
+        }
+        public ICollection<string> Items => this.items;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.InvalidPlanetName);
+                }
+                this.name = value;
+            }
+        }
+    }
+}
