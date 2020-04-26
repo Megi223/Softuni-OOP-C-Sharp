bd0165a7cd293e0a103997394ddfc031b3860c37
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-04-26 13:32:49 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 15 August 2019/P01+P02-Structure and Business Logic/Repositories/AstronautRepository.cs
@@ -0,0 +1,43 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Repositories
+
+{
+    public class AstronautRepository : IRepository<IAstronaut>
+    {
+        private List<IAstronaut> models;
+        public AstronautRepository()
+        {
+            this.models = new List<IAstronaut>();
+        }
+        public IReadOnlyCollection<IAstronaut> Models => this.models.AsReadOnly();
+
+        public void Add(IAstronaut model)
+        {
+            this.models.Add(model);
+        }
+
+        public IAstronaut FindByName(string name)
+        {
+            IAstronaut wantedAstronaut = this.models.FirstOrDefault(a => a.Name == name);
+            return wantedAstronaut;
+        }
+
+        public bool Remove(IAstronaut model)
+        {
+            IAstronaut wantedAstronaut = this.models.FirstOrDefault(a => a == model);
+            if (wantedAstronaut == null)
+            {
+                return false;
+            }
+            this.models.Remove(wantedAstronaut);
+            return true;
+
+        }
+    }
+}
