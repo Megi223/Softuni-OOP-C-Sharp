0c73fe18a7c573460b0392677e2eb6b7d05644ca
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-02 12:28:46 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 18 April 2019/P01+P02-Structure and Business Logic/Core/Engine.cs
@@ -0,0 +1,64 @@
+﻿using PlayersAndMonsters.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Core
+{
+    public class Engine : IEngine
+    {
+        private IManagerController managerController;
+        public Engine()
+        {
+            this.managerController = new ManagerController();
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine().Split();
+                if (input[0] == "Exit")
+                {
+                    Environment.Exit(0);
+                }
+                try
+                {
+                    if (input[0] == "AddPlayer")
+                    {
+                        string type = input[1];
+                        string name = input[2];
+                        Console.WriteLine(managerController.AddPlayer(type,name));
+                    }
+                    else if (input[0] == "AddCard")
+                    {
+                        string type = input[1];
+                        string name = input[2];
+                        Console.WriteLine(managerController.AddCard(type, name));
+                    }
+                    else if (input[0] == "AddPlayerCard")
+                    {
+                        string username = input[1];
+                        string cardName = input[2];
+                        Console.WriteLine(managerController.AddPlayerCard(username,cardName));
+                    }
+                    else if (input[0] == "Fight")
+                    {
+                        string attacker = input[1];
+                        string enemy = input[2];
+                        Console.WriteLine(managerController.Fight(attacker, enemy));
+                    }
+                    else if (input[0] == "Report")
+                    {
+                        
+                        Console.WriteLine(managerController.Report());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //throw;
+                }
+            }
+        }
+    }
+}
---
+++ Exams/C# OOP Exam - 18 April 2019/P01+P02-Structure and Business Logic/Core/ManagerController.cs
@@ -0,0 +1,94 @@
+﻿namespace PlayersAndMonsters.Core
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Contracts;
+    using PlayersAndMonsters.Common;
+    using PlayersAndMonsters.Models.BattleFields;
+    using PlayersAndMonsters.Models.Cards;
+    using PlayersAndMonsters.Models.Cards.Contracts;
+    using PlayersAndMonsters.Models.Players;
+    using PlayersAndMonsters.Models.Players.Contracts;
+    using PlayersAndMonsters.Repositories;
+
+    public class ManagerController : IManagerController
+    {
+        private PlayerRepository playerRepository;
+        private CardRepository cardRepository;
+        public ManagerController()
+        {
+            this.playerRepository = new PlayerRepository();
+            this.cardRepository = new CardRepository();
+        }
+
+        public string AddPlayer(string type, string username)
+        {
+            IPlayer player = null;
+            if (type == "Beginner")
+            {
+                player = new Beginner(new CardRepository(), username);
+            }
+            else if (type == "Advanced")
+            {
+                player = new Advanced(new CardRepository(), username);
+            }
+            this.playerRepository.Add(player);
+            return String.Format(ConstantMessages.SuccessfullyAddedPlayer, type, username);
+        }
+
+        public string AddCard(string type, string name)
+        {
+            ICard card = null;
+            if (type == "Trap")
+            {
+                card = new TrapCard(name);
+            }
+            else if (type == "Magic")
+            {
+                card = new MagicCard(name);
+            }
+            this.cardRepository.Add(card);
+            return String.Format(ConstantMessages.SuccessfullyAddedCard, type, name);
+        }
+
+       
+        public string AddPlayerCard(string username, string cardName)
+        {
+            IPlayer wantedPlayer = this.playerRepository.Players.FirstOrDefault(p => p.Username == username);
+            ICard wantedCard = this.cardRepository.Cards.FirstOrDefault(c => c.Name == cardName);
+            if ( wantedPlayer!=null)
+            {
+                wantedPlayer.CardRepository.Add(wantedCard);
+                return String.Format(ConstantMessages.SuccessfullyAddedPlayerWithCards, cardName, username);
+            }
+            return "";
+        }
+
+        public string Fight(string attackUser, string enemyUser)
+        {
+            IPlayer attacker = this.playerRepository.Players.FirstOrDefault(p => p.Username == attackUser);
+            IPlayer enemy = this.playerRepository.Players.FirstOrDefault(p => p.Username == enemyUser);
+            BattleField battleField = new BattleField();
+            battleField.Fight(attacker, enemy);
+            return String.Format(ConstantMessages.FightInfo, attacker.Health, enemy.Health);
+
+
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var player in this.playerRepository.Players)
+            {
+                sb.AppendLine(String.Format(ConstantMessages.PlayerReportInfo, player.Username, player.Health, player.CardRepository.Count));
+                foreach (var card in player.CardRepository.Cards)
+                {
+                    sb.AppendLine(String.Format(ConstantMessages.CardReportInfo, card.Name, card.DamagePoints));
+                }
+                sb.AppendLine(ConstantMessages.DefaultReportSeparator);
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
