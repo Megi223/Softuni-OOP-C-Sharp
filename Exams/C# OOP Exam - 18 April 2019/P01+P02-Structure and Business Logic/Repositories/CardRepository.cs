6af651654ef9e57bbb024dfad3ce1d11a04dc68b
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-03 16:24:38 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 18 April 2019/P01+P02-Structure and Business Logic/Repositories/CardRepository.cs
@@ -0,0 +1,50 @@
+﻿using PlayersAndMonsters.Models.Cards.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayersAndMonsters.Repositories
+{
+    public class CardRepository : ICardRepository
+    {
+        private List<ICard> cards;
+        public CardRepository()
+        {
+            this.cards = new List<ICard>();
+        }
+        public int Count => this.cards.Count;
+
+        public IReadOnlyCollection<ICard> Cards => this.cards.AsReadOnly();
+
+        public void Add(ICard card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentException("Card cannot be null!");
+            }
+            ICard someCard = this.cards.FirstOrDefault(c => c.Name == card.Name);
+            if (someCard != null)
+            {
+                throw new ArgumentException($"Card {card.Name} already exists!");
+            }
+            this.cards.Add(card);
+        }
+
+        public ICard Find(string name)
+        {
+            ICard card = this.cards.FirstOrDefault(c => c.Name == name);
+            return card;
+        }
+
+        public bool Remove(ICard card)
+        {
+            if (card == null)
+            {
+                throw new ArgumentException("Card cannot be null!");
+            }
+            return this.cards.Remove(card);
+        }
+    }
+}
---
+++ Exams/C# OOP Exam - 18 April 2019/P01+P02-Structure and Business Logic/Repositories/PlayerRepository.cs
@@ -0,0 +1,53 @@
+﻿using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayersAndMonsters.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private List<IPlayer> players;
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+        }
+        public int Count => this.players.Count;
+
+        public IReadOnlyCollection<IPlayer> Players => this.players.AsReadOnly();
+
+        public void Add(IPlayer player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException("Player cannot be null");
+            }
+            IPlayer somePlayer = this.players.FirstOrDefault(p => p.Username == player.Username);
+            if (somePlayer != null)
+            {
+                throw new ArgumentException($"Player {player.Username} already exists!");
+            }
+            this.players.Add(player);
+
+        }
+
+        public IPlayer Find(string username)
+        {
+            IPlayer player = this.players.FirstOrDefault(p => p.Username == username);
+            return player;
+
+        }
+
+        public bool Remove(IPlayer player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException("Player cannot be null");
+
+            }
+            return this.players.Remove(player);
+        }
+    }
+}
