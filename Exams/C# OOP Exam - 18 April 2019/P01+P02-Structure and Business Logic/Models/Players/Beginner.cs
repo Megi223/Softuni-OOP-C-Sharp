ca2a900811d2cee5f0e9bc49c3dd328958d26f3e
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-04 11:54:46 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 18 April 2019/P01+P02-Structure and Business Logic/Models/Players/Beginner.cs
@@ -0,0 +1,15 @@
+﻿using PlayersAndMonsters.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Models.Players
+{
+    public class Beginner : Player
+    {
+        private const int DEF_BEGINNER_HEALTH = 50;
+        public Beginner(ICardRepository cardRepository, string username) : base(cardRepository, username, DEF_BEGINNER_HEALTH)
+        {
+        }
+    }
+}
---
+++ Exams/C# OOP Exam - 18 April 2019/P01+P02-Structure and Business Logic/Models/Players/Player.cs
@@ -0,0 +1,76 @@
+﻿using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Models.Players
+{
+    public abstract class Player : IPlayer
+    {
+        private string username;
+        private int health;
+     
+        protected Player(ICardRepository cardRepository, string username, int health)
+        {
+            this.CardRepository = cardRepository;
+            this.Username = username;
+            this.Health = health;
+        }
+        public string Username 
+        {
+            get
+            {
+                return this.username;
+            }
+            private set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    //TODO: Check if here there should be space after the dot.
+                    throw new ArgumentException("Player's username cannot be null or an empty string.");
+                }
+                this.username = value;
+            }
+        }
+
+        public ICardRepository CardRepository { get; private set; }
+
+
+        public int Health 
+        {
+            get
+            {
+                return this.health;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Player's health bonus cannot be less than zero.");
+                }
+                this.health = value;
+            }
+        }
+
+        //TODO: Check if zero should be included-logically yes
+        public bool IsDead => this.Health <= 0;
+
+        public void TakeDamage(int damagePoints)
+        {
+            if (damagePoints < 0)
+            {
+                throw new ArgumentException("Damage points cannot be less than zero.");
+            }
+
+            if (this.Health - damagePoints >= 0)
+            {
+                this.Health -= damagePoints;
+            }
+            else
+            {
+                this.Health = 0;
+            }
+        }
+    }
+}
