b63884688fa57c341cddbfd63ae781d37beb0962
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-04 11:51:30 +0300

    Add files via upload

---
+++ Exams/C# OOP Exam - 18 April 2019/P01+P02-Structure and Business Logic/Models/Cards/MagicCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Models.Cards
+{
+    public class MagicCard : Card
+    {
+        private const int DEF_DAMAGE_POINTS = 5;
+        private const int DEF_HEALTH_POINTS = 80;
+        public MagicCard(string name) : base(name, DEF_DAMAGE_POINTS, DEF_HEALTH_POINTS)
+        {
+        }
+    }
+}
---
+++ Exams/C# OOP Exam - 18 April 2019/P01+P02-Structure and Business Logic/Models/Cards/TrapCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Models.Cards
+{
+    public class TrapCard : Card
+    {
+        private const int DEF_DAMAGE_POINTS = 120;
+        private const int DEF_HEALTH_POINTS = 5;
+
+        public TrapCard(string name) : base(name, DEF_DAMAGE_POINTS, DEF_HEALTH_POINTS)
+        {
+        }
+    }
+}
