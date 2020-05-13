ddbfa50e1c00179065e54db276b102b7110da290
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-13 09:39:53 +0300

    Add files via upload

---
+++ Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //RaceMotorcycle m = new RaceMotorcycle(100, 20);
+            //Console.WriteLine(m.DefaultFuelConsumption);
+            //FamilyCar c = new FamilyCar(100, 20);
+            
+        }
+    }
+}
