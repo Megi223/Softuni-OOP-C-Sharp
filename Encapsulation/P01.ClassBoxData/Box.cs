8cd342b501cfc94dab07c04945ea51cd3bbca5fc
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-16 12:38:43 +0300

    Add files via upload

---
+++ Encapsulation/P01.ClassBoxData/Box.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01ClassBoxData
+{
+    public class Box
+    {
+        private double height;
+        private double width;
+        private double length;
+
+
+        public Box(double length,double width,double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+        public double Height 
+        {
+            get { return this.height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    
+                    //Console.WriteLine("Height cannot be zero or negative.");
+                    throw new ArgumentException("Height cannot be zero or negative.");
+
+                }
+                this.height = value;
+            }
+        }
+        public double Width
+        {
+            get { return this.width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    //Console.WriteLine("Width cannot be zero or negative.");
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                    
+                }
+                this.width = value;
+            }
+        }
+        public double Length
+        {
+            get { return this.length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    
+                   //Console.WriteLine("Length cannot be zero or negative.");
+                    throw new Exception("Length cannot be zero or negative.");
+                    
+                }
+                this.length = value;
+            }
+        }
+
+        public string GetSurfaceArea()
+        {
+            double result = 2 * this.Length * this.Width + 2 * this.Width * this.Height + 2 * this.Length * this.Height;
+            return String.Format("{0:0.00}", result);
+        }
+        public string GetLateralSurfaceArea()
+        {
+            double result =  2 * this.Width * this.Height + 2 * this.Length * this.Height;
+            return String.Format("{0:0.00}", result);
+        }
+        public string GetVolume()
+        {
+            double result = this.Height * this.Length * this.Width;
+            return String.Format("{0:0.00}", result);
+
+        }
+    }
+}
---
+++ Encapsulation/P01.ClassBoxData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P01ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            try
+            {
+                var box = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea()}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea()}");
+                Console.WriteLine($"Volume - {box.GetVolume()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //return;
+            }
+            
+
+            
+
+
+        }
+    }
+}
