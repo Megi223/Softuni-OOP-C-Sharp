399bd13c2a63d37562ffc82987f81e69bbba74ee
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-17 14:51:42 +0300

    Add files via upload

---
+++ Encapsulation/P03.ShoppingSpree/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+
+        public Person(string name,decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bag = new List<Product>();
+        }
+
+        public string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        public decimal Money 
+        {
+            get
+            {
+                return this.money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+        public IReadOnlyCollection<Product> Bag => this.bag.AsReadOnly();
+        public void AddProductToBag(Product product)
+        {
+            this.bag.Add(product);
+        }
+        public void DecreaseMoney(decimal costOfDecreasing)
+        {
+            this.Money -= costOfDecreasing;
+        }
+    }
+}
---
+++ Encapsulation/P03.ShoppingSpree/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03ShoppingSpree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+            List<string> peopleInput = Console.ReadLine()
+                 .Split(";")
+                 .ToList();
+            
+            List<string> productsInput = Console.ReadLine()
+                 .Split(";")
+                 .ToList();
+            for (int i = 0; i < peopleInput.Count; i++)
+            {
+                string[] currentPerson = peopleInput[i].Split("=");
+                try
+                {
+                    Person person = new Person(currentPerson[0], decimal.Parse(currentPerson[1]));
+                    people.Add(person);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            for (int i = 0; i < productsInput.Count; i++)
+            {
+                string[] currentProduct = productsInput[i].Split("=",StringSplitOptions.RemoveEmptyEntries);
+                if (currentProduct.Length <=1) { continue; }
+                try
+                {
+                    Product product = new Product(currentProduct[0], decimal.Parse(currentProduct[1]));
+                    products.Add(product);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command!="END")
+            {
+                string[] commandToArr = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = commandToArr[0];
+                string productName = commandToArr[1];
+                Person targetPerson = null;
+                Product targetProduct = null;
+                foreach (var person in people)
+                {
+                    if (person.Name == name)
+                    {
+                        targetPerson = person;
+                    }
+                }
+                foreach (var product in products)
+                {
+                    if (product.Name == productName)
+                    {
+                        targetProduct = product;
+                    }
+                }
+                if (targetPerson.Money >= targetProduct.Cost)
+                {
+                    Console.WriteLine($"{targetPerson.Name} bought {targetProduct.Name}");
+                    targetPerson.AddProductToBag(targetProduct);
+                    targetPerson.DecreaseMoney(targetProduct.Cost);
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"{targetPerson.Name} can't afford {targetProduct.Name}");
+                }
+                command = Console.ReadLine();
+            }
+            foreach (var person in people)
+            {
+                if (person.Bag.Count == 0)
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+                else
+                {
+                    List<string> boughtProducts = new List<string>();
+                    foreach (var item in person.Bag)
+                    {
+                        boughtProducts.Add(item.Name);
+                    }
+                    string personsBag = string.Join(", ", boughtProducts);
+                    Console.WriteLine($"{person.Name} - {personsBag}");
+
+
+                }
+
+            }
+
+        }
+    }
+}
