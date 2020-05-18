6e8fe206a5a05cb2ad9b8ef9429042d67d3d54fb
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-18 11:59:53 +0300

    Add files via upload

---
+++ Encapsulation/P05.FootballTeamGenerator/Common/GlobalConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05FootballTeamGenerator.Common
+{
+    public static class GlobalConstants
+    {
+        public static string InvalidStatExceptionMessage = "{0} should be between 0 and 100.";
+        public static string InvalidNameExceptionMessage = "A name should not be empty.";
+        public static string RemovingMissingPlayerExceptionMessage = "Player {0} is not in {1} team.";
+        public static string MissingTeamExceptionMessage = "Team {0} does not exist.";
+    }
+}
