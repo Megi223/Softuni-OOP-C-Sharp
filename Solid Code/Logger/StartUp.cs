40fac73c06f7104ff35d43b188a71f00f0c1f05a
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-22 12:34:23 +0300

    Add files via upload

---
+++ Solid Code/Logger/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Logger.Core;
+using Logger.Factories;
+using Logger.Core.Contracts;
+using Logger.Models.Contracts;
+
+namespace Logger
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int appendersCount = int.Parse(Console.ReadLine());
+            ICollection<IAppender> appenders = new List<IAppender>();
+
+            ParseAppendersInput(appendersCount, appenders);
+
+            ILogger logger = new Logger.Models.Logger(appenders);
+
+            IEngine engine = new Engine(logger);
+            engine.Run();
+
+        }
+
+        private static void ParseAppendersInput(int appendersCount, ICollection<IAppender> appenders)
+        {
+            AppenderFactory appenderFactory = new AppenderFactory();
+            for (int i = 0; i < appendersCount; i++)
+            {
+                string[] appenderArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string appenderType = appenderArgs[0];
+                string layoutType = appenderArgs[1];
+                string level = "INFO";
+
+                if (appenderArgs.Length == 3)
+                {
+                    level = appenderArgs[2];
+                }
+                try
+                {
+                    IAppender appender = appenderFactory.ProduceAppender(appenderType, layoutType, level);
+                    appenders.Add(appender);
+                }
+                catch (ArgumentException ae)
+                {
+
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}
