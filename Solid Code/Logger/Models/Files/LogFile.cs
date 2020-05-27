12131bf32a9a04702c1e21c4076ad401ab7fccff
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-27 11:01:34 +0300

    Add files via upload

---
+++ Solid Code/Logger/Models/Files/LogFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+using Logger.Common;
+using Logger.Models.Contracts;
+using Logger.Models.Enumerations;
+using Logger.Models.IOManagement;
+
+namespace Logger.Models.Files
+{
+    public class LogFile : IFile
+    {
+        private IOManager IOManager;
+
+        public LogFile(string folderName,string fileName)
+        {
+            this.IOManager = new IOManager(folderName, fileName);
+            this.IOManager.EnsureDirectoryAndFileExist();
+        }
+        public string Path => this.IOManager.CurrentFilePath;
+
+        public long Size => this.GetFileSize();
+
+        public string Write(ILayout layout, IError error)
+        {
+            string format = layout.Format;
+            DateTime dateTime = error.DateTime;
+            string message = error.Message;
+            Level level = error.Level;
+            string formattedMessage = String.Format(format, dateTime.ToString(GlobalConstants.DATE_FORMAT, CultureInfo.InvariantCulture), message, level.ToString())+Environment.NewLine;
+            return formattedMessage;
+        }
+
+        private long GetFileSize()
+        {
+            string text = File.ReadAllText(this.Path);
+            long size = text.Where(c => Char.IsLetter(c))
+                .Sum(c => c);
+            return size;
+        }
+    }
+}
