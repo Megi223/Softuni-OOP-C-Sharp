9ae9b3027d1d5532e64aae977f67a02232c64794
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-30 12:53:12 +0300

    Add files via upload

---
+++ Polymorphism/01.Vehicles/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01Vehicles
+{
+    public class Car : Vehicle
+    {
+        private const double INCREASED_FUEL_CONSUMPTION = 0.9;
+        public Car(double fuelQuantity, double litersPerKm) : base(fuelQuantity, litersPerKm)
+        {
+        }
+
+        public override void Drive(double distance)
+        {
+            double litersPerKmNew = this.LitersPerKm + INCREASED_FUEL_CONSUMPTION;
+            double neededFuel = distance * litersPerKmNew;
+            if (neededFuel <= this.FuelQuantity)
+            {
+                this.FuelQuantity -= neededFuel;
+                Console.WriteLine($"Car travelled {distance} km");
+            }
+            else
+            {
+                Console.WriteLine("Car needs refueling");
+            }
+        }
+
+        public override void Refuel(double fuel)
+        {
+            this.FuelQuantity += fuel;
+        }
+    }
+}
