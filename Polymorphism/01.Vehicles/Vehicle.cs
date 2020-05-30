26b44d34f2cc72550bb6ed0082ee514d7ca5cec1
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-30 12:54:37 +0300

    Add files via upload

---
+++ Polymorphism/01.Vehicles/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01Vehicles
+{
+   public abstract class Vehicle
+    {
+        public Vehicle(double fuelQuantity,double litersPerKm)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.LitersPerKm = litersPerKm;
+        }
+        public double FuelQuantity { get; set; }
+        public double LitersPerKm { get; set; }
+
+        public abstract void Drive(double distance);
+        public abstract void Refuel(double fuel);
+
+
+    }
+}
