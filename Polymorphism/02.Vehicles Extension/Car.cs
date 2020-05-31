06e0701458bb60056a1df8bd4a15aa77cb161da6
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-31 12:34:26 +0300

    Add files via upload

---
+++ Polymorphism/02.Vehicles Extension/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01P02Vehicles
+{
+    public class Car : Vehicle
+    {
+        private const double INCREASED_FUEL_CONSUMPTION = 0.9;
+
+        public Car(double fuelQuantity, double litersPerKm, double tankCapacity) : base(fuelQuantity, litersPerKm, tankCapacity)
+        {
+            
+        }
+
+        public override void Drive(double distance)
+        {
+            double litersPerKmNew = this.LitersPerKm + INCREASED_FUEL_CONSUMPTION;
+            double neededFuel = distance * litersPerKmNew;
+            if (neededFuel <= this.FuelQuantity)
+            {
+                this.FuelQuantity -= neededFuel;
+                Console.WriteLine($"Car travelled {distance} km");
+            }
+            else
+            {
+                Console.WriteLine("Car needs refueling");
+            }
+        }
+
+        public override void DriveEmpty(double distance)
+        {
+            
+        }
+
+        public override void Refuel(double fuel)
+        {
+            if (fuel <= 0)
+            {
+                Console.WriteLine("Fuel must be a positive number");
+            }
+            else
+            {
+                double allFuel = this.FuelQuantity + fuel;
+                if (allFuel > this.TankCapacity)
+                {
+                    Console.WriteLine($"Cannot fit {fuel} fuel in the tank");
+                }
+                else
+                {
+                    this.FuelQuantity += fuel;
+                }
+            }
+            
+            
+        }
+    }
+}
