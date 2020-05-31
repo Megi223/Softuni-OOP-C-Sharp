8d94c2348a3cab5eec41d7d8a4d83c38e3fee283
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-05-31 12:33:38 +0300

    Add files via upload

---
+++ Polymorphism/02.Vehicles Extension/Bus.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01P02Vehicles
+{
+    public class Bus : Vehicle
+    {
+        private const double INCREASED_FUEL_CONSUMPTION = 1.4;
+        public Bus(double fuelQuantity, double litersPerKm, double tankCapacity) : base(fuelQuantity, litersPerKm, tankCapacity)
+        {
+            
+        }
+
+        public override void Drive(double distance)
+        {
+            double litersPerKmNew = this.LitersPerKm + INCREASED_FUEL_CONSUMPTION;
+            double neededFuel = distance * litersPerKmNew;
+            if (neededFuel <= this.FuelQuantity)
+            {
+                this.FuelQuantity -= neededFuel;
+                Console.WriteLine($"Bus travelled {distance} km");
+            }
+            else
+            {
+                Console.WriteLine("Bus needs refueling");
+            }
+        }
+       
+
+        public override void DriveEmpty(double distance)
+        {
+            double neededFuel = distance * LitersPerKm;
+            if (neededFuel <= this.FuelQuantity)
+            {
+                this.FuelQuantity -= neededFuel;
+                Console.WriteLine($"Bus travelled {distance} km");
+            }
+            else
+            {
+                Console.WriteLine("Bus needs refueling");
+            }
+        }
+
+        public override void Refuel(double fuel)
+        {
+            if (fuel <= 0)
+            {
+                Console.WriteLine("Fuel must be a positive number");
+            }
+            else
+            {
+                double allFuel = this.FuelQuantity + fuel;
+                if (allFuel > this.TankCapacity)
+                {
+                    Console.WriteLine($"Cannot fit {fuel} fuel in the tank");
+                }
+                else
+                {
+                    this.FuelQuantity += fuel;
+                }
+            }
+            
+        }
+    }
+}
