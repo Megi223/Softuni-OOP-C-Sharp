02705ba1c2d1eb6363444b8ddeee4252b989d213
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-06 14:10:49 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/StartUp.cs
@@ -0,0 +1,21 @@
+﻿
+using P03Raiding.Core;
+using P03Raiding.Core.Contracts;
+using P03Raiding.IO;
+using P03Raiding.IO.Contracts;
+
+namespace P03Raiding
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+
+            IEngine engine = new Engine(reader,writer);
+            engine.Run();
+           
+        }
+    }
+}
