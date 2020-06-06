35b8ad2d93897bf0e0b92a3714164904fd889593
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-06 14:11:54 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/Factories/DruidFactory.cs
@@ -0,0 +1,22 @@
+﻿
+using P03Raiding.Models;
+
+namespace P03Raiding.Factories
+{
+    class DruidFactory : Factory
+    {
+        private const int POWER= 80;
+        private string name;
+        private int power;
+
+        public DruidFactory(string name)
+        {
+            this.name = name;
+            this.power = POWER;
+        }
+        public override BaseHero CreateHero(string name)
+        {
+            return new Druid(name);
+        }
+    }
+}
