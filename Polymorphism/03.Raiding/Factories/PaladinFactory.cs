4c8b958122c78cde0f1c4c60ad2848d8c83b28fd
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-07 13:11:31 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/Factories/PaladinFactory.cs
@@ -0,0 +1,22 @@
+﻿
+using P03Raiding.Models;
+
+namespace P03Raiding.Factories
+{
+    public class PaladinFactory : Factory
+    {
+        private const int POWER = 100;
+        private string name;
+        private int power;
+        public PaladinFactory(string name)
+        {
+            this.name = name;
+            this.power = POWER;
+        }
+
+        public override BaseHero CreateHero(string name)
+        {
+            return new Paladin(name);
+        }
+    }
+}
