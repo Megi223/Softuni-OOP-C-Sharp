41e43f43f0662a1ee57eefd6455c3362a7d73472
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-07 13:12:01 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/Factories/RogueFactory.cs
@@ -0,0 +1,22 @@
+﻿
+using P03Raiding.Models;
+
+namespace P03Raiding.Factories
+{
+    public class RogueFactory : Factory
+    {
+        private const int POWER = 80;
+        private string name;
+        private int power;
+        public RogueFactory(string name)
+        {
+            this.name = name;
+            this.power = POWER;
+                
+        }
+        public override BaseHero CreateHero(string name)
+        {
+            return new Rogue(name);
+        }
+    }
+}
