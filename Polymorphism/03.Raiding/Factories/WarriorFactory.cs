f590b4db9774e7b2e41759d661778432f1ae337a
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-08 12:48:23 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/Factories/WarriorFactory.cs
@@ -0,0 +1,22 @@
+﻿
+using P03Raiding.Models;
+
+namespace P03Raiding.Factories
+{
+    public class WarriorFactory : Factory
+    {
+        private const int POWER = 100;
+        private string name;
+        private int power;
+        public WarriorFactory(string name)
+        {
+            this.name = name;
+            this.power = POWER;
+        }
+
+        public override BaseHero CreateHero(string name)
+        {
+            return new Warrior(name);
+        }
+    }
+}
