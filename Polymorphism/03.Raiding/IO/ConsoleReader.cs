6ead4ea6afef788df6ca652331a60b1704018783
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-09 00:06:11 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/IO/ConsoleReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using P03Raiding.IO.Contracts;
+
+namespace P03Raiding.IO
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
