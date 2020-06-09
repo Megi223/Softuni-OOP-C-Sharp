a936e57213a9d42fc09149be12ae9e0b847c2b8f
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-10 01:37:13 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/IO/ConsoleWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using P03Raiding.IO.Contracts;
+
+namespace P03Raiding.IO
+{
+    class ConsoleWriter : IWriter
+    {
+        public void Write(string text)
+        {
+            Console.Write(text);
+        }
+
+        public void WriteLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
