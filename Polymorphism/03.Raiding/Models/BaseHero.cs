6c0f1fe4f01b1e7b989bfa14b2d14be2b87ed3e3
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-10 01:38:11 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/Models/BaseHero.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace P03Raiding.Models
+{
+    public abstract class BaseHero
+    {
+        public BaseHero(string name)
+        {
+            this.Name = name;
+            
+        }
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public abstract string CastAbility();
+    }
+}
