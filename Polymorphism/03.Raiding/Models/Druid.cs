e05c3f91b79651d2c96059978273ba7166d38dca
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-11 11:08:41 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/Models/Druid.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace P03Raiding.Models
+{
+    public class Druid : BaseHero
+    {
+        public Druid(string name) : base(name)
+        {
+            this.Power = 80;
+        }
+
+        public override string CastAbility()
+        {
+            return $"Druid - {this.Name} healed for {this.Power}";
+        }
+    }
+}
