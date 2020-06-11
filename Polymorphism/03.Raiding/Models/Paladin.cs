0ec038fe3326d4644f3e66898a4239300fdc1215
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-11 11:09:05 +0300

    Add files via upload

---
+++ Polymorphism/03.Raiding/Models/Paladin.cs
@@ -0,0 +1,17 @@
+﻿
+namespace P03Raiding.Models
+{
+    public class Paladin : BaseHero
+    {
+        public Paladin(string name) : base(name)
+        {
+            this.Power = 100;
+
+        }
+
+        public override string CastAbility()
+        {
+            return $"Paladin - {this.Name} healed for {this.Power}";
+        }
+    }
+}
