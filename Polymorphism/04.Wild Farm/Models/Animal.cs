77a0f9e32606eea8279afeeb52f756105f247945
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-14 13:43:32 +0300

    Add files via upload

---
+++ Polymorphism/04.Wild Farm/Models/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04WildFarm.Models
+{
+   public abstract class Animal
+    {
+        public Animal(string name,double weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.FoodEaten = 0;
+        }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int FoodEaten { get; set; }
+
+        public abstract string ProduceSound();
+        public abstract void Eat(string foodType,int foodQuantity);
+    }
+}
