f3ca3e509f55234edbee15c13bf09e53b2c118ec
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-16 01:19:12 +0300

    Add files via upload

---
+++ Polymorphism/04.Wild Farm/Models/Dog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04WildFarm.Models
+{
+    public class Dog : Mammal
+    {
+        private const double INCREASE_IN_WEIGHT = 0.4;
+        public Dog(string name, double weight, string livingRegion) : base(name, weight, livingRegion)
+        {
+        }
+
+        public override void Eat(string foodType, int foodQuantity)
+        {
+            if (foodType == "Meat")
+            {
+                double weightIncrease = foodQuantity * INCREASE_IN_WEIGHT;
+                this.Weight += weightIncrease;
+                this.FoodEaten += foodQuantity;
+
+            }
+            else
+            {
+                throw new ArgumentException($"{this.GetType().Name} does not eat {foodType}!");
+            }
+        }
+
+        public override string ProduceSound()
+        {
+            return "Woof!";
+        }
+    }
+}
