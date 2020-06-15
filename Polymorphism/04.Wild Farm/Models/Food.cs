f26322ddfe24aa138b5849b22d648dfbb65f8c07
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-16 01:21:06 +0300

    Add files via upload

---
+++ Polymorphism/04.Wild Farm/Models/Food.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04WildFarm.Models
+{
+    public abstract class Food
+    {
+        public Food(int quantity)
+        {
+            this.Quantity = quantity;
+        }
+        public int Quantity { get; set; }
+    }
+}
