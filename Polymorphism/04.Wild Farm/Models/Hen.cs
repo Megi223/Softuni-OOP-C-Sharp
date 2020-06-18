88543b8d1544a0b3bc348ad111b7ba93809fa303
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-18 13:40:25 +0300

    Add files via upload

---
+++ Polymorphism/04.Wild Farm/Models/Hen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04WildFarm.Models
+{
+    public class Hen : Bird
+    {
+        private const double INCREASE_IN_WEIGHT = 0.35;
+        public Hen(string name, double weight, double wingSize) : base(name, weight, wingSize)
+        {
+        }
+
+        public override void Eat(string foodType, int foodQuantity)
+        {
+            double weightIncrease = foodQuantity * INCREASE_IN_WEIGHT;
+            this.Weight += weightIncrease;
+            this.FoodEaten += foodQuantity;
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Cluck";
+        }
+    }
+}
