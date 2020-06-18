b86a7154a58958a2a9cc4e2ed97297956b7937af
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-18 13:41:41 +0300

    Add files via upload

---
+++ Polymorphism/04.Wild Farm/Models/Meat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04WildFarm.Models
+{
+    public class Meat : Food
+    {
+        public Meat(int quantity) : base(quantity)
+        {
+        }
+    }
+}
