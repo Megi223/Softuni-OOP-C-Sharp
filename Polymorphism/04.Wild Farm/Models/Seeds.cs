d026b172bdfe96abcb0c150bf87998f287bdabad
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-19 16:29:05 +0300

    Add files via upload

---
+++ Polymorphism/04.Wild Farm/Models/Seeds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04WildFarm.Models
+{
+    public class Seeds : Food
+    {
+        public Seeds(int quantity) : base(quantity)
+        {
+        }
+    }
+}
