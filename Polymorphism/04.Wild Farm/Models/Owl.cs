afdb50c496aaf30e7be3941e7ae1a68296dcd8da
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-19 16:28:41 +0300

    Add files via upload

---
+++ Polymorphism/04.Wild Farm/Models/Owl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04WildFarm.Models
+{
+    public class Owl : Bird
+    {
+        private const double INCREASE_IN_WEIGHT = 0.25;
+
+        public Owl(string name, double weight, double wingSize) : base(name, weight, wingSize)
+        {
+        }
+
+        public override void Eat(string foodType, int foodQuantity)
+        {
+            if (foodType == "Meat")
+            {
+                double weightIncrease = foodQuantity * INCREASE_IN_WEIGHT;
+                this.Weight += weightIncrease;
+                this.FoodEaten += foodQuantity;
+            }
+            else
+            {
+                throw new ArgumentException($"{this.GetType().Name} does not eat {foodType}!");
+            }
+        }
+
+        public override string ProduceSound()
+        {
+            return "Hoot Hoot";
+        }
+    }
+}
