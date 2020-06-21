bd85ae829360a3ee0a0fc24eee927fba9214e595
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-21 08:56:42 +0300

    Add files via upload

---
+++ Reflection and Attributes/01.Command Pattern/Core/Engine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandPattern.Core.Contracts;
+using CommandPattern.Models;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+        public Engine(ICommandInterpreter interpreter)
+        {
+            this.commandInterpreter = interpreter;
+        }
+        public void Run()
+        {
+
+            string input;
+            while ((input=Console.ReadLine())!="Exit")
+            {
+                
+                string result = commandInterpreter.Read(input);
+                Console.WriteLine(result);
+               
+            }
+            
+        }
+    }
+}
