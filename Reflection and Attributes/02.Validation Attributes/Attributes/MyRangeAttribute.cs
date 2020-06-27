4eea8731c3c6d12aa631478d681e848ff004a11a
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-27 12:36:39 +0300

    Add files via upload

---
+++ Reflection and Attributes/02.Validation Attributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute:MyValidationAttribute
+    {
+        private int min;
+        private int max;
+        public MyRangeAttribute(int minValue,int maxValue)
+        {
+            this.min = minValue;
+            this.max = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if(obj is Int32)
+            {
+                int number = (int)obj;
+                if(number<=this.max && number >= this.min)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
