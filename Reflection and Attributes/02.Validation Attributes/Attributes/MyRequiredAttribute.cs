fbf60241a795ae6b9a6887059761de5ef7e4e323
Megi223 <61914037+Megi223@users.noreply.github.com>
2020-06-28 12:24:12 +0300

    Add files via upload

---
+++ Reflection and Attributes/02.Validation Attributes/Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRequiredAttribute : MyValidationAttribute
+    {
+
+        public override bool IsValid(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            //Type[] objType = Assembly.GetExecutingAssembly().GetTypes();
+            //foreach (var item in objType)
+            //{
+            //    var sth = item.GetProperties();
+            //    foreach (var secondItem in sth)
+            //    {
+            //        var sthelse = secondItem.GetCustomAttributes().FirstOrDefault(x => x.GetType().Name == nameof(MyRequiredAttribute));
+            //        if (sth == null)
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //}
+            return true;
+        }
+    }
+}
